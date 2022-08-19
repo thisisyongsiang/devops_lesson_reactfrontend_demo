651c6fd6ab9c5a24a3f302201655b956d43f031c
thisisyongsiang <ongys1992@gmail.com>
2022-08-19 16:41:58 +0800

    add env

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
+const apiURL=process.env.REACT_APP_REACT_APP_APIURL;
 
 function App() {
   const [custId, setCustId] = useState('');
@@ -11,8 +12,8 @@ function App() {
     var requestOptions = {
       method: "GET",
     };
-    //var url = "http://localhost:3000/customer/id?cid=" + custId;
-    var url = "https://nus-backend.herokuapp.com/customer/id?cid=" + custId;
+    var url = apiURL+"customer/id?cid=" + custId;
+    //var url = "https://nus-backend.herokuapp.com/customer/id?cid=" + custId;
     console.log(url); 
     fetch(url, requestOptions)
       .then((response) => response.json())
@@ -24,8 +25,8 @@ function App() {
     var requestOptions = {
       method: "GET",
     };
-    //var url = "http://localhost:3000/customer/all";
-    var url = "https://nus-backend.herokuapp.com/customer/all";
+    var url = apiURL+"customer/all";
+    //var url = "https://nus-backend.herokuapp.com/customer/all";
     
     console.log(url);
     fetch(url, requestOptions)
